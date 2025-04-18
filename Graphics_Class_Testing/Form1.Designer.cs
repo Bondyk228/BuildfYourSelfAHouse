df74612b1c813620875de1d8b2a0d49a2a85fe9d
Pusdestroyer228 <Pusdestroyer228@LAPTOP-OWNED-BY>
2025-04-18 13:41:35 +0300

    final product

--- Graphics_Class_Testing/Form1.Designer.cs
+++ Graphics_Class_Testing/Form1.Designer.cs
@@ -33,12 +33,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // PictureBox1
             // 
-            this.PictureBox1.Location = new System.Drawing.Point(100, 149);
+            this.PictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.PictureBox1.Location = new System.Drawing.Point(300, 244);
+            this.PictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PictureBox1.Name = "PictureBox1";
             this.PictureBox1.Size = new System.Drawing.Size(600, 400);
             this.PictureBox1.TabIndex = 0;
@@ -46,9 +50,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(328, 12);
+            this.button1.Location = new System.Drawing.Point(495, 18);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 36);
+            this.button1.Size = new System.Drawing.Size(210, 55);
             this.button1.TabIndex = 1;
             this.button1.Text = "Build :)";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,36 +61,53 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(328, 54);
+            this.button2.Location = new System.Drawing.Point(495, 83);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 39);
+            this.button2.Size = new System.Drawing.Size(210, 60);
             this.button2.TabIndex = 2;
             this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.Form1_Load);
+            this.button2.Click += new System.EventHandler(this.BtnClear_press);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(328, 99);
+            this.button3.Location = new System.Drawing.Point(495, 153);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(144, 39);
+            this.button3.Size = new System.Drawing.Size(210, 60);
             this.button3.TabIndex = 3;
             this.button3.Text = "Burn :(";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Form1_Load);
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.BtnPrsBURN);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Graphics_Class_Testing.Properties.Resources.house_burn;
+            this.pictureBox2.Location = new System.Drawing.Point(-12, -29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1203, 928);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1178, 844);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.PictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Build yourself a house";
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -97,6 +119,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Graphics_Class_Testing/Form1.cs
+++ Graphics_Class_Testing/Form1.cs
@@ -17,14 +17,29 @@ namespace Graphics_Class_Testing
         {
             InitializeComponent();
             g = PictureBox1.CreateGraphics();
-
+            
         }
         private void Form1_Load(object sender, EventArgs e)
         {
             button3.Visible = true;
             Bitmap bitmap = new Bitmap(PictureBox1.Width, PictureBox1.Height);
-            g.DrawRectangle(new Pen(Color.Black, 3), 10, 50, 100, 100);
-            g.DrawLine(new Pen(Color.Red, 3), 10, 10, 110, 110);   
+            g.DrawRectangle(new Pen(Color.Black, 3), 150, 110, 100, 100);
+            g.DrawLine(new Pen(Color.Red, 3), 150, 110, 200, 60);
+            g.DrawLine(new Pen(Color.Red, 3), 200, 60, 250, 110);
+            g.DrawRectangle(new Pen(Color.Brown, 3), 175, 150, 50, 60);
+
+        }
+
+        private void BtnClear_press(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            PictureBox1.Invalidate();
+            button3.Visible = false;
+        }
+        private void BtnPrsBURN(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            button3.Visible = false;
         }
     }
 }
